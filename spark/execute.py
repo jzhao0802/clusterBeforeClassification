da009770a4d2a254b997d66ba26d605da0455978
Lichao Wang (Dell laptop) <lichao.wang@uk.imshealth.com>
2016-12-16 16:42:43 +0000

    Following things changed for testing:
    1. deploy mode: cluster
    2. data: 10% of BI
    
    Also note that current the aggregated predictions over all folds are not cached.
    
    The hope is to reproduce the error that the computation of precision at a given recall fails for the aggregated predictions with a smaller amount of data, so that it's easier to test the fix.

--- spark/execute.py
+++ spark/execute.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("spark-submit --deploy-mode client --master yarn --num-executors 7 --executor-cores 16 --executor-memory 16g clustering_analysis.py &> log.txt &")
+os.system("spark-submit --deploy-mode cluster --master yarn --num-executors 7 --executor-cores 16 --executor-memory 16g clustering_analysis.py &> log.txt &")
